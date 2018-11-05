a11ccc6f9305fd4b8d03e417abba4fbea28da0cb
Diederik Krols <diederik_krols@hotmail.com>
2018-11-05 19:59:16 +0100

    Added gallery page

---
+++ XamlBrewer.Uwp.ArcPathSample/Views/GalleryPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace XamlBrewer.Uwp.ArcPathSample
+{
+    public sealed partial class GalleryPage : Page
+    {
+        public GalleryPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
