19f99371c6a11c04c38ff236715738f39c14fc83
Diederik Krols <diederik_krols@hotmail.com>
2018-11-06 20:49:54 +0100

    Description and proper credits

--- XamlBrewer.Uwp.ArcPathSample/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.ArcPathSample/ViewModels/ShellViewModel.cs
@@ -11,7 +11,7 @@ namespace Mvvm
             Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("HomeIcon"), Text = "Home", NavigationDestination = typeof(HomePage) });
             Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("TopLayerIcon"), Text = "XAML", NavigationDestination = typeof(ArcSegmentPage) });
             Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("MiddleLayerIcon"), Text = "Composition", NavigationDestination = typeof(Win2DPage) });
-            Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("DonutIcon"), Text = "Circle Segment", NavigationDestination = typeof(CircleSegmentPage) });
+            Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("DonutIcon"), Text = "Ring Segment", NavigationDestination = typeof(CircleSegmentPage) });
             Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("PacmanIcon"), Text = "Gallery", NavigationDestination = typeof(GalleryPage) });
             Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("SquaresIcon"), Text = "Squares", NavigationDestination = typeof(SquaresPage) });
             SecondMenu.Add(new MenuItem() { Glyph = Icon.GetIcon("InfoIcon"), Text = "About", NavigationDestination = typeof(AboutPage) });
