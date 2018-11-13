e4be0ee2593440a476bcf213a5ae15a4a657c378
Diederik Krols <diederik_krols@hotmail.com>
2018-11-13 20:22:16 +0100

    Removed an obsolete view model.

--- XamlBrewer.Uwp.ArcPathSample/Views/Win2DPage.xaml.cs
+++ XamlBrewer.Uwp.ArcPathSample/Views/Win2DPage.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using Windows.UI.Xaml.Controls;
-using XamlBrewer.Uwp.ArcPathSample.ViewModels;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace XamlBrewer.Uwp.ArcPathSample
 {
     public sealed partial class Win2DPage : Page
     {
-        private EllipticalArcViewModel _viewModel = new EllipticalArcViewModel();
-
         public Win2DPage()
         {
             this.InitializeComponent();
         }
-
-        public EllipticalArcViewModel ViewModel => _viewModel;
     }
 }
